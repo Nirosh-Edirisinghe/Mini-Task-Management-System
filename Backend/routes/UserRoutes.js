91ded7f340422c8756f906716405903c16e87e65
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2026-06-05 14:48:20 +0530

    setup cloudinary configuration for store images and create route conftollr function for it

--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -1,10 +1,13 @@
 import express from "express"
 import { authorizeRoles, Authuser } from "../middleware/AuthMiddleware.js";
-import { createUser, getAllUsers } from "../controllers/UserController.js";
+import { createUser, getAllUsers, getUserProfile, updateUserProfile } from "../controllers/UserController.js";
+import upload from "../middleware/multer.js";
 
 const usersRouter = express.Router();
 
 usersRouter.get("/all-users", Authuser, authorizeRoles("ADMIN", "USER"), getAllUsers);
 usersRouter.post("/add-users", Authuser, authorizeRoles("ADMIN"), createUser);
+usersRouter.get('/profile', Authuser, getUserProfile)
+usersRouter.put("/update-profile", Authuser, upload.single("image"), updateUserProfile);
 
 export default usersRouter;
