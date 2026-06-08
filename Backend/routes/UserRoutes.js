2843cbf6cc34f94513ed4af827e4c36becd903db
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2026-06-08 22:19:29 +0530

    create update email functionality

--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { authorizeRoles, Authuser } from "../middleware/AuthMiddleware.js";
-import { createUser, getAllUsers, getUserProfile, updateUserProfile } from "../controllers/UserController.js";
+import { createUser, getAllUsers, getUserProfile, updateUserEmail, updateUserProfile } from "../controllers/UserController.js";
 import upload from "../middleware/multer.js";
 
 const usersRouter = express.Router();
@@ -9,5 +9,6 @@ usersRouter.get("/all-users", Authuser, authorizeRoles("ADMIN", "USER"), getAllU
 usersRouter.post("/add-users", Authuser, authorizeRoles("ADMIN"), createUser);
 usersRouter.get('/profile', Authuser, getUserProfile)
 usersRouter.put("/update-profile", Authuser, upload.single("image"), updateUserProfile);
+usersRouter.put('/update-email', Authuser, updateUserEmail)
 
 export default usersRouter;
